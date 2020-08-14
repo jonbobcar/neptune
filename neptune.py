b0aaaa84f655d9ad44648189db9c3e0cef29c2fb
jonbobcar <jon.carstens@gmail.com>
2020-08-14 10:53:45 -0700

    Update neptune.py

--- neptune.py
+++ neptune.py
@@ -1,27 +1,68 @@
-import urllib.request, json
+import urllib.request, json, csv, datetime
 
-with urllib.request.urlopen("http://nptriton.cqproject.net/game/4845520221372416/full") as url:
+game_number = 4845520221372416
+url = "http://nptriton.cqproject.net/game/" + str(game_number) + "/full"
+
+with urllib.request.urlopen(url) as url:
     data = json.loads(url.read().decode())
 
-# print("Weapons Level: ", data["players"]["0"])
-# print(type(data))
+class Player():
+    def __init__(self, uid):
+        uid = str(uid)
+        self.alias = data["players"][uid]["alias"]
+        self.systems = data["players"][uid]["total_stars"]
+        self.ships = data["players"][uid]["total_strength"]
+        self.carriers = data["players"][uid]["total_fleets"]
+        self.scanning = data["players"][uid]["tech"]["scanning"]["level"]
+        self.range = data["players"][uid]["tech"]["propulsion"]["level"]
+        self.terraforming = data["players"][uid]["tech"]["terraforming"]["level"]
+        self.research = data["players"][uid]["tech"]["research"]["level"]
+        self.weapons = data["players"][uid]["tech"]["weapons"]["level"]
+        self.banking = data["players"][uid]["tech"]["banking"]["level"]
+        self.manufacturing = data["players"][uid]["tech"]["manufacturing"]["level"]
 
-print("Alias", "Systems", "Ships", "Carriers", "Upgrades")
+players = []
+for player in range(len(data["players"])):
+    players.append(Player(player))
 
-upgrades = ["scanning", "propulsion", "terraforming", "research", "weapons", "banking", "manufacturing"]
+fieldnames = [
+    "alias",
+    "total_stars",
+    "total_strength",
+    "total_fleets",
+    "scanning",
+    "propulsion",
+    "terraforming",
+    "research",
+    "weapons",
+    "banking",
+    "manufacturing"
+]
+
+file_name = "neptune.csv"
+
+with open(file_name, "w") as write_file:
+    csv_writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+write_line = []
 
 for player in range(len(data["players"])):
     player = str(player)
-    upgrade_array = []
-    for upgrade in upgrades:
-        upgrade_array.append(data["players"][player]["tech"][upgrade]["level"])
-    print(data["players"][player]["alias"],
-    data["players"][player]["total_stars"],
-    data["players"][player]["total_strength"],
-    data["players"][player]["total_fleets"],
-    upgrade_array)
-
-# num = 0
-# numS = str(num)
-
-# print(data["players"][numS]["tech"]["weapons"])
+    write_line.append({
+        "alias":            data["players"][player]["alias"],
+        "total_stars":      data["players"][player]["total_stars"],
+        "total_strength":   data["players"][player]["total_strength"],
+        "total_fleets":     data["players"][player]["total_fleets"],
+        "scanning":         data["players"][player]["tech"]["scanning"]["level"],
+        "propulsion":       data["players"][player]["tech"]["propulsion"]["level"],
+        "terraforming":     data["players"][player]["tech"]["terraforming"]["level"],
+        "research":         data["players"][player]["tech"]["research"]["level"],
+        "weapons":          data["players"][player]["tech"]["weapons"]["level"],
+        "banking":          data["players"][player]["tech"]["banking"]["level"],
+        "manufacturing":    data["players"][player]["tech"]["manufacturing"]["level"],
+    })
+with open(file_name, "a") as write_file:
+    csv_writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+    for row in write_line:
+        csv_writer.writerow(row)
