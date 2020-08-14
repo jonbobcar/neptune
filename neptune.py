12c6d4555b345b23b45b77b23ae20227d001c166
jonathon <jon.carstens@gmail.com>
2020-08-14 06:38:52 -0700

    working toward version 0.1

--- neptune.py
+++ neptune.py
@@ -6,18 +6,20 @@ with urllib.request.urlopen("http://nptriton.cqproject.net/game/4845520221372416
 # print("Weapons Level: ", data["players"]["0"])
 # print(type(data))
 
-print("Alias", "Systems", "Ships", "Carriers")
+print("Alias", "Systems", "Ships", "Carriers", "Upgrades")
 
 upgrades = ["scanning", "propulsion", "terraforming", "research", "weapons", "banking", "manufacturing"]
 
 for player in range(len(data["players"])):
     player = str(player)
+    upgrade_array = []
+    for upgrade in upgrades:
+        upgrade_array.append(data["players"][player]["tech"][upgrade]["level"])
     print(data["players"][player]["alias"],
     data["players"][player]["total_stars"],
     data["players"][player]["total_strength"],
-    data["players"][player]["total_fleets"])
-    for upgrade in upgrades:
-        print(upgrade, data["players"][player]["tech"][upgrade]["level"])
+    data["players"][player]["total_fleets"],
+    upgrade_array)
 
 # num = 0
 # numS = str(num)
