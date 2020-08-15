854c3c1728e7f5bb4e9fb8a0ef4f37d97562cfd3
jonathon <jon.carstens@gmail.com>
2020-08-15 15:11:54 -0700

    Adding neptune to html

---
+++ neptune_to_html.py
@@ -0,0 +1,29 @@
+import urllib.request, json, csv, datetime, pathlib, os
+
+path = str(pathlib.Path(__file__).parent.absolute()) + "/"
+
+with open(path + "trades.txt", "r") as f:
+    trades = f.readlines()
+
+with open(path + "neptune.csv", "r") as f:
+    positions = f.readlines()
+
+with open(path + "index.html", "w") as f:
+    f.write("<!DOCTYPE html>\n")
+    f.write("<html>\n")
+    f.write("<body>\n")
+    f.write("<h1>\n")
+    f.write("Positions")
+    f.write("</h1>\n")
+    for line in positions:
+        f.write("<p>" + line + "</p>")
+    f.write("<h1>\n")
+    f.write("System Trades\n")
+    f.write("</h1>\n")
+    f.write("<p1>\n")
+    f.write("Most Recent First")
+    for line in reversed(trades):
+        f.write("<p>" + line + "</p>")
+    f.write("</p1>\n")
+    f.write("</body>\n")
+    f.write("</html>\n")
