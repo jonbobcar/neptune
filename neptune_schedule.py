071b284e41f8b1286b356bb5f3a736df9a91ab35
jonbobcar <jon.carstens@gmail.com>
2020-08-17 11:50:20 -0700

    Create neptune_schedule.py

---
+++ neptune_schedule.py
@@ -0,0 +1,16 @@
+import time
+import schedule
+
+# cron job to copy index.html:
+# */5 * * * * cp /home/jonathon/neptune/index.html /var/www/html/jonbobcar.com/public_html/
+
+def job():
+    import neptune
+    import neptune_to_html
+    print("Ran the job")
+
+schedule.every().hour.at(":54").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
