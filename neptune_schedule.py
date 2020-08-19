2a5791aee7d591fcc9f4663fe54e3724b3e33897
Jonathon Carstens <jon.carstens@gmail.com>
2020-08-19 05:41:03 -0700

    Updated

--- neptune_schedule.py
+++ neptune_schedule.py
@@ -9,7 +9,14 @@ def job():
     import neptune
     import stats_plotter
     import neptune_to_html
-    import neptune_move
+    # import neptune_move
+
+    shutil.copy("/home/jonathon/neptune/index.html", "/var/www/html/jonbobcar.com/public_html/index.html")
+    shutil.copy("/home/jonathon/neptune/stars.png", "/var/www/html/jonbobcar.com/public_html/stars.png")
+    shutil.copy("/home/jonathon/neptune/ships.png", "/var/www/html/jonbobcar.com/public_html/ships.png")
+    shutil.copy("/home/jonathon/neptune/economy.png", "/var/www/html/jonbobcar.com/public_html/economy.png")
+    shutil.copy("/home/jonathon/neptune/industry.png", "/var/www/html/jonbobcar.com/public_html/industry.png")
+    shutil.copy("/home/jonathon/neptune/science.png", "/var/www/html/jonbobcar.com/public_html/science.png")
     print("Ran the job")
     
 schedule.every().hour.at(":54").do(job)
