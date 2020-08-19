dc854c65a8eb1840e53cc1a3498939b7fc7df5a3
Jonathon Carstens <jon.carstens@gmail.com>
2020-08-18 17:09:16 -0700

    updated

--- neptune_schedule.py
+++ neptune_schedule.py
@@ -1,5 +1,6 @@
 import time
 import schedule
+import shutil
 
 # cron job to copy index.html:
 # */5 * * * * cp /home/jonathon/neptune/index.html /var/www/html/jonbobcar.com/public_html/
@@ -9,6 +10,14 @@ def job():
     import stats_plotter
     import neptune_to_html
     print("Ran the job")
+
+    shutil.move("/home/jonathon/neptune/index.html", "/var/www/html/jonbobcar.com/public_html/index.html")
+    shutil.move("/home/jonathon/neptune/stars.png", "/var/www/html/jonbobcar.com/public_html/stars.png")
+    shutil.move("/home/jonathon/neptune/ships.png", "/var/www/html/jonbobcar.com/public_html/ships.png")
+    shutil.move("/home/jonathon/neptune/economy.png", "/var/www/html/jonbobcar.com/public_html/economy.png")
+    shutil.move("/home/jonathon/neptune/industry.png", "/var/www/html/jonbobcar.com/public_html/industry.png")
+    shutil.move("/home/jonathon/neptune/science.png", "/var/www/html/jonbobcar.com/public_html/science.png")
+    
 
 schedule.every().hour.at(":54").do(job)
 
